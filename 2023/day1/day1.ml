b3aeca8b581953ef48133150011021489de3dc18
Brandon Woodford <brandon.woodford.work@gmail.com>
2023-12-04 07:15:53 -0600

    Day 1 part 2 work in progress

--- 2023/day1/day1.ml
+++ 2023/day1/day1.ml
@@ -1,6 +1,24 @@
 open Base
 open Stdio
 
+let word_value = function
+| "one" | "1" -> 1
+| "two" | "2" -> 2
+| "three" | "3" -> 3
+| "four" | "4" -> 4
+| "five" | "5" -> 5
+| "six" | "6" -> 6
+| "seven" | "7" -> 7
+| "eight" | "8" -> 8
+| "nine" | "9" -> 9
+| _ -> 0
+  
+let find_all_digits str = 
+  let pattern = Re.compile (Re.str "(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9)" ) in
+  let matches = Re.exec pattern str in
+  matches
+
+
 let rec find_cali_val chars = 
   match chars with
   | [] -> failwith "no digit found"
@@ -22,13 +40,29 @@ let part1 strings =
   in
   string_loop strings 0
 
+let get_val string =
+  let matches = find_all_digits string in
+  let l = Re.Group.get matches 0 in
+  let r = Re.Group.get matches (Re.Group.nb_groups matches) in
+  10 * (word_value l) + (word_value r)
+
+let part2 strings = 
+  let rec string_loop strings acc = 
+    match strings with
+    | [] ->  acc
+    | h :: t -> string_loop t (acc + (get_val h))
+  in
+  string_loop strings 0
+
 let solve filename = 
   let content = In_channel.read_all filename in
 
+  (*
   content
   |> String.split_lines
-  |> part1
+  |> part1 
   |> printf "part1: %d\n";
+  *)
 
   content
   |> String.split_lines
@@ -36,4 +70,4 @@ let solve filename =
   |> printf "part2: %d\n"
 
 
-let () = solve "input.txt"
+let () = solve "example2.txt"
