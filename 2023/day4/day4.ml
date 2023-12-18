fffc38dbbd7b62c4fde34f4f50706313cb158595
Brandon Woodford <brandon.woodford.work@gmail.com>
2023-12-18 13:01:13 -0600

    Finish Day4 Part1

--- 2023/day4/day4.ml
+++ 2023/day4/day4.ml
@@ -7,11 +7,12 @@ type card = {
 }
 
 let rec get_game_numbers game pos games =
-  let regex = Re.Str.regexp "\d+" in
-  let _ = Re.Str.search_forward regex game pos in
-  match String.length game with
-  | 0 -> games
-  | _ -> get_game_numbers game (Re.Str.match_end ()) (Int.of_string (Re.Str.matched_string game) :: games)
+  match pos with
+  | pos when pos >= (String.length game)-1 -> games
+  | _ -> 
+    let regex = Re.Str.regexp "[0-9]+" in
+    let _ = Re.Str.search_forward regex game pos in
+    get_game_numbers game (Re.Str.match_end ()) (Int.of_string (Re.Str.matched_string game) :: games)
 
 let parse lines = 
   let get_games game =
@@ -22,8 +23,24 @@ let parse lines =
   in
   List.map ~f:(get_games) lines
 
+let rec get_matches theirs ours matches =
+  match theirs with
+    | [] -> matches
+    | h :: t -> 
+      if List.exists ~f:(fun num -> num = h) ours then 
+        get_matches t ours (matches + 1)
+      else 
+        get_matches t ours matches
 
-let part1 card = 1 
+let part1 cards =
+  let get_total_matches acc card =
+    let matches = get_matches card.theirs card.ours 0 in
+    if matches > 0 then
+      acc + 2 ** (matches - 1)
+    else
+      acc + 0
+  in
+  List.fold_left ~f:(get_total_matches) ~init:0 cards
 
 let part2 card = 1
 
@@ -36,4 +53,4 @@ let solve filename =
   part1 card |> printf "part1: %d\n";
   part2 card |> printf "part2: %d\n"
 
-let () = solve "example1.txt"
+let () = solve "input.txt"
